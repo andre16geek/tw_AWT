4f902185469f31ffd12c9a630fd242b6074eb0cb
andre <andreantoine0016@gmail.com>
2020-07-27 21:09:39 +0200

    delete & patch update

--- routes/api/tws.js
+++ routes/api/tws.js
@@ -10,18 +10,39 @@ var tws = [];
 
 Router.get('/', testMiddleware, (req, res) => {
     // res.status(200).send('GET Tws');
-    res.status(200).json(tws);
+    // res.status(200).json(tws);
+    Tw.find()
+        .lean()
+        .exec()
+        .then( tws => {res.status(200).json(tws);})
+        .catch(err => { 
+            error = err;
+            console.error(error);
+        })
 })
 
 Router.get('/:twId', (req, res) => {
     twId = req.params.twId;
 
-    tw = tws.filter((tw) => {
-        return tw.id == twId;
-    });
+    // tw = tws.filter((tw) => {
+    //     return tw.id == twId;
+    // });
 
-    // res.status(200).send('GET Tw Id: ' + twId);
-    res.status(200).json(tw);
+    // // res.status(200).send('GET Tw Id: ' + twId);
+    // res.status(200).json(tw);
+
+    Tw.find({
+        id:twId  
+    })
+    .lean()
+    .exec()
+    .then(tw => {
+        res.status(200).json(tw);
+    })
+    .catch(err => { 
+        error = err;
+        console.error(error);
+    })
 })
 
 Router.post('/', (req, res) => {
@@ -48,24 +69,43 @@ Router.post('/', (req, res) => {
 Router.delete('/:twId', (req, res) => {
     twId = req.params.twId;
 
-    tws = tws.filter((tw) => {
-        return tw.id != twId;
-    })
+    // tws = tws.filter((tw) => {
+    //     return tw.id != twId;
+    // })
 
-    res.status(200).json(tws);
+    // res.status(200).json(tws);
+    Tw.remove({
+        id:twID
+    })
+    .exec()
+    .then(result => {
+        res.status(200).json({'message':'Deleted !'});
+    })
+    .catch(err => { 
+        error = err;
+        console.error(error);
+    })
 })
 
 Router.patch('/:twId', (req, res) => {
     twId = req.params.twId;
     message = req.body.message;
-
-    tws.filter((tw) => {
-        if (tw.id == twId) {
-            tw.message = message
-        }
-    });
-
-    res.status(200).json(tws);
+    if (req.body.message && req.body.message != "") {
+        Tw.update({ _id: twId }, {
+                $set: {
+                    message: req.body.message,
+                }
+            })
+            .exec()
+            .then(tw => {
+                res.status(200).json(tw);
+            })
+            .catch(err => {
+                res.status(500).json({ error: err });
+            });
+    } else {
+        res.status(500).json({ error: 'Complete all fields' });
+    }
 })
 
 module.exports = Router;
