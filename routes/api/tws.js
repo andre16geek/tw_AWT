ffb0d852ee30cfb6f31c413b2a6d322234a69841
andre <andreantoine0016@gmail.com>
2020-07-29 12:57:43 +0200

    list tweets

--- routes/api/tws.js
+++ routes/api/tws.js
@@ -4,47 +4,41 @@ const mongoose = require('mongoose');
 const Router = express.Router();
 
 const Tw = require('../../models/Tw');
-const testMiddleware = require('../../middelwares/test');
 
-var tws = [];
-
-Router.get('/', testMiddleware, (req, res) => {
-    // res.status(200).send('GET Tws');
-    // res.status(200).json(tws);
+// This is the route to have all the tws
+Router.get('/', (req, res) => {
     Tw.find()
         .lean()
         .exec()
-        .then( tws => {res.status(200).json(tws);})
-        .catch(err => { 
+        .then(tws => {
+            res.status(200).json(tws);
+        })
+        .catch(err => {
             error = err;
             console.error(error);
-        })
+        });
 })
 
+// this is the route to have one specific tw
 Router.get('/:twId', (req, res) => {
     twId = req.params.twId;
 
-    // tw = tws.filter((tw) => {
-    //     return tw.id == twId;
-    // });
-
-    // // res.status(200).send('GET Tw Id: ' + twId);
-    // res.status(200).json(tw);
-
     Tw.find({
-        id:twId  
+        _id: twId
     })
-    .lean()
-    .exec()
-    .then(tw => {
-        res.status(200).json(tw);
-    })
-    .catch(err => { 
-        error = err;
-        console.error(error);
-    })
+        .lean()
+        .exec()
+        .then(tw => {
+            res.status(200).json(tw);
+        })
+        .catch(err => {
+            error = err;
+            console.error(error);
+        });
 })
 
+
+//this is the route to create a tw
 Router.post('/', (req, res) => {
     console.log(req.body.message);
     
@@ -66,30 +60,28 @@ Router.post('/', (req, res) => {
     }
 })
 
+// this is the route to delete a tw
 Router.delete('/:twId', (req, res) => {
     twId = req.params.twId;
 
-    // tws = tws.filter((tw) => {
-    //     return tw.id != twId;
-    // })
-
-    // res.status(200).json(tws);
     Tw.remove({
-        id:twID
+        _id: twId
     })
-    .exec()
-    .then(result => {
-        res.status(200).json({'message':'Deleted !'});
-    })
-    .catch(err => { 
-        error = err;
-        console.error(error);
-    })
+        .exec()
+        .then(result => {
+            res.status(200).json({ 'message': 'Suppression du twitt !' });
+        })
+        .catch(err => {
+            error = err;
+            console.error(error);
+        });
 })
 
+// this is the route to update a tw
 Router.patch('/:twId', (req, res) => {
     twId = req.params.twId;
     message = req.body.message;
+
     if (req.body.message && req.body.message != "") {
         Tw.update({ _id: twId }, {
                 $set: {
@@ -104,7 +96,7 @@ Router.patch('/:twId', (req, res) => {
                 res.status(500).json({ error: err });
             });
     } else {
-        res.status(500).json({ error: 'Complete all fields' });
+        res.status(500).json({ error: 'Merci de remplire tous les champs' });
     }
 })
 
